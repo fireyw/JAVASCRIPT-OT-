045f4bc8e6339577e97562ba36fada684413dc15
kywsc <fireyw@gmail.com>
2020-03-13 12:15:30 +0900

    react-redux

---
+++ opentutorial/react/redux/src/components/DisplayNumber.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class DisplayNumber extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Add Number</h1>
+        <input type="text" value={this.props.number}></input>
+      </div>
+    );
+  }
+}
+
+export default DisplayNumber;
