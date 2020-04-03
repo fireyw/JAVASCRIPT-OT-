39b066c6cda9ee5dd4c54d8b37d0112b2ed2c0a6
kywsc <fireyw@gmail.com>
2020-04-04 08:51:18 +0900

    react App change

--- opentutorial/react/proxy/reactapp/src/index.js
+++ opentutorial/react/proxy/reactapp/src/index.js
@@ -1,24 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <input
-          type="button"
-          value="get Data"
-          onClick={function() {
-            fetch("/data.json")
-              .then(function(result) {
-                return result.json();
-              })
-              .then(function(json) {
-                console.log(json);
-              });
-          }}
-        ></input>
-      </div>
+      <input
+        type="button"
+        value="get Data"
+        onClick={function() {
+          fetch("/data.json")
+            .then(function(result) {
+              return result.json();
+            })
+            .then(function(json) {
+              console.log(json);
+            });
+        }}
+      ></input>
     );
   }
 }
